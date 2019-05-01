bab230de9781301becd2dc8abaa8a3aff211d2ce
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2019-05-01 09:56:39 -0400

    part 3 is now working

--- lessons/part_01/cli.py
+++ lessons/part_01/cli.py
@@ -5,9 +5,15 @@ import click
 
 @click.command()
 @click.argument("names", nargs=-1)
-def cli(names):
+@click.option("--greeting", "-g", default="Hello")
+@click.option("--question/--no-question")
+def cli(names, greeting, question):
+    if question:
+        punctuation = "?"
+    else:
+        punctuation = "!"
     for name in names:
-        print("Hello, {}!".format(name))
+        print("{}, {}{}".format(greeting, name, punctuation))
 
 
 if __name__ == "__main__":
